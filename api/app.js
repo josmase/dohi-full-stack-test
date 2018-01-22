5b7deae2eb1b74e0ab2b6aab5379bf322f8c3dec
Jonas Lundberg <josmase@users.noreply.github.com>
2018-01-22 22:42:45 +0100

    Fix error handling

--- api/app.js
+++ api/app.js
@@ -22,14 +22,14 @@ app.use((req, res, next) => {
 });
 
 // error handler
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-    // render the error page
     res.status(err.status || 500);
-    res.send(err)
+    res.json(res.locals);
+    next(err);
 });
 
 module.exports = app;
--- api/routes/bundle.js
+++ api/routes/bundle.js
@@ -5,22 +5,28 @@ const database = require('../database');
 router.get('/bundle/', (req, res) => {
     database.getBundles()
         .then(data => res.send(data))
-        .catch(err => {
-            throw new Error(err)
-        })
+        .catch(err => next(err))
 });
 
 /* GET bundle. */
 router.get('/bundle/:id', (req, res) => {
     database.getBundles(req.params.id)
         .then(data => res.send(data))
-        .catch(err => {
-            throw new Error(err)
-        })
+        .catch(err => next(err))
 });
 /* DELETE bundle */
-router.delete('/bundle/:id', (req, res) => {
-    database.deleteBundle(req.params.id).then(data => res.send(data.affectedRows))
+router.delete('/bundle/:id', (req, res, next) => {
+    database.deleteBundle(req.params.id)
+        .then(data => {
+            if (data.affectedRows === 0) {
+                const err = new Error('Bundle not found: ' + req.params.id);
+                err.status = 404;
+                next(err);
+            } else {
+                res.send(data)
+            }
+        })
+        .catch(err => next(err))
 });
 
 module.exports = router;
