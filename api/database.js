f967fdc2f74d2c39f335d0d2add4bb510eddf0a3
Jonas Lundberg <josmase@users.noreply.github.com>
2018-01-22 21:39:01 +0100

    Add support for getting bundles with and without using id

--- api/database.js
+++ api/database.js
@@ -33,3 +33,61 @@ function query(sql, inserts) {
     })
 }
 
+/**
+ * Gets the bundle matching the bundle id. If no id is given all bundles will be returned. Every bundle contains paths and places.
+ * @param bundleID Optional. The id of the bundle to get.
+ * @returns {Promise<*>} An array of bundles.
+ */
+async function getBundles(bundleID) {
+    let sql = "SELECT id, name, image info FROM bundle";
+
+    if (bundleID != null) {
+        sql += " WHERE id=?";
+    }
+
+    const bundles = await query(sql, bundleID);
+    for (let i = 0; i < bundles.length; i++) {
+        bundles[i].paths = await getBundleExtras(bundles[i].id);
+    }
+    return bundles;
+}
+
+/**
+ * Gets tha paths and the places for each path. WHere the paths are part of the bundle.
+ * @param bundleID The id of the bundle to get paths from.
+ * @returns {Promise<*>} All the paths and places.
+ */
+async function getBundleExtras(bundleID) {
+    const paths = await getPaths(bundleID);
+    await addPlacesToPaths(paths);
+    return paths;
+}
+
+/**
+ * Gets all the paths of a bundle.
+ * @param bundleID The id of the bundle to get paths for.
+ * @returns {*} All the paths for the bundle as a promise.
+ */
+function getPaths(bundleID) {
+    const sql = "SELECT id, name, info, length, duration, image FROM path WHERE bundleID=?";
+    return query(sql, [bundleID])
+}
+
+/**
+ * Iterates all the paths and adds its places.
+ * @param paths All the paths to add to.
+ * @returns {Promise<void>}
+ */
+async function addPlacesToPaths(paths) {
+    for (let i = 0; i < paths.length; i++) {
+        paths[i].places = await getPlaces(paths[i].id)
+    }
+}
+
+function getPlaces(pathID) {
+    const sql = "SELECT id, name, info, image, radius FROM place WHERE pathID=?";
+    return query(sql, [pathID]);
+}
+
+
+exports.getBundles = getBundles;
